bb35c2730bc6f0734544270609bff2b34b8bac07
Gökay Akçay <Gokay1904@gmail.com>
2023-03-19 22:56:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,117 @@
-from webbrowser import Grail
-
 import pygame as pg
+import numpy as np
+from OpenGL.GL import *
+import ctypes
+from OpenGL.GL.shaders import compileProgram,compileShader
 import moderngl as mgl
 import sys
 
-class GraphicsEngine:
-    def __init__(self, win_size=(1600,900)):
+from OpenGL.raw.GLU import gluPerspective
+
+
+class App:
+    def __init__(self):
 
         pg.init()
-        self.WIN_SIZE = win_size
-
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION,3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
-
-        pg.display.set_mode(self.WIN_SIZE,flags=pg.OPENGL | pg.DOUBLEBUF)
-
-        self.ctx = mgl.create_context()
-
+        display = (640,480)
+        pg.display.set_mode(display,pg.OPENGL|pg.DOUBLEBUF)
         self.clock = pg.time.Clock()
 
-    def check_events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                pg.quit()
-                sys.exit()
 
-    def render(self):
-        self.ctx.clear(color=(0.08,0.16,0.18))
+        self.mainLoop()
 
-        pg.display.flip()
+    def mainLoop(self):
+        display = (640, 480)
 
-    def run(self):
-        while True:
-            self.check_events()
-            self.render()
-            self.clock.tick(60)
+        glClearColor(0.1, 0.2, 0.2, 1)
+
+        gluPerspective(45,display[0]/display[1],0.1,50.0)
+
+        glTranslatef(0.0, 0.0, -5)
+
+        glRotatef(0, 0, 0, 0)
+
+        running = True
+        while(running):
+
+            for event in pg.event.get():
+                if(event.type == pg.QUIT):
+                    running = False
+            glRotatef(1, 3, 1, 1)
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+            Cube()
+            pg.display.flip()
+            pg.time.wait(10)
+
+        self.quit()
+
+
+
+    def quit(self):
+        pg.quit()
+
+
+
+class Cube:
+    def __init__(self):
+
+        #x y z r g b
+        self.verticies = (
+            (1,-1,-1),
+            (1,1,-1),
+            (-1, 1, -1),
+            (-1, -1, -1),
+            (1, -1, 1),
+            (1, 1, 1),
+            (-1, -1, 1),
+            (-1, 1, 1)
+        )
+
+        self.edges = (
+            (0,1),
+            (0,3),
+            (0,4),
+            (2,1),
+            (2,3),
+            (2,7),
+            (6,3),
+            (6,4),
+            (6,7),
+            (5,1),
+            (5,4),
+            (5,7)
+        )
+
+
+        glBegin(GL_LINES)
+        for edge in self.edges:
+            for vertex in edge:
+                print(self.verticies[vertex])
+                glVertex3fv(self.verticies[vertex])
+        glEnd()
+
+        #self.vertices = np.array(self.vertices,dtype = np.float32)
+        #self.vertex_count = 3
+#
+        #self.vao = glGenVertexArrays(1)
+        #glBindVertexArray(self.vao)
+        #self.vbo = glGenBuffers(1)
+        #glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        #glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+        #glEnableVertexAttribArray(0)
+        #glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        #glEnableVertexAttribArray(1)
+        #glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(1))
+
+
+
+    def destroy(self):
+
+        glDeleteVertexArrays(1,(self.vao,))
+        glDeleteBuffers(1,(self.vbo,))
+
 
 if __name__ == '__main__':
-    app = GraphicsEngine()
-    app.run()
+    app = App()
+
 
